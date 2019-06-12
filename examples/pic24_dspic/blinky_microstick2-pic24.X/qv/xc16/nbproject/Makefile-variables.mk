f765fcd368702e72fabf37ce74295334e0e8e1e5
QL <info@state-machine.com>
2019-06-12 11:17:08 -0400

    6.5.1

---
+++ examples/pic24_dspic/blinky_microstick2-pic24.X/qv/xc16/nbproject/Makefile-variables.mk
@@ -0,0 +1,27 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=xc16.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/xc16.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=xc16.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/xc16.tar
+# release configuration
+CND_ARTIFACT_DIR_release=dist/release/production
+CND_ARTIFACT_NAME_release=xc16.production.hex
+CND_ARTIFACT_PATH_release=dist/release/production/xc16.production.hex
+CND_PACKAGE_DIR_release=${CND_DISTDIR}/release/package
+CND_PACKAGE_NAME_release=xc16.tar
+CND_PACKAGE_PATH_release=${CND_DISTDIR}/release/package/xc16.tar
+# spy configuration
+CND_ARTIFACT_DIR_spy=dist/spy/production
+CND_ARTIFACT_NAME_spy=xc16.production.hex
+CND_ARTIFACT_PATH_spy=dist/spy/production/xc16.production.hex
+CND_PACKAGE_DIR_spy=${CND_DISTDIR}/spy/package
+CND_PACKAGE_NAME_spy=xc16.tar
+CND_PACKAGE_PATH_spy=${CND_DISTDIR}/spy/package/xc16.tar
