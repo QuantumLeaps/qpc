07c764993d4a40b94075ea1cd14e770fcdeac175
Marco Casaroli <marco.casaroli@gmail.com>
2020-04-01 16:08:05 +0200

    proper fix from upstream

--- src/qf/qf_time.c
+++ src/qf/qf_time.c
@@ -394,6 +394,7 @@ bool QTimeEvt_disarm(QTimeEvt * const me) {
     }
     else { /* the time event was already disarmed automatically */
         wasArmed = false;
+		me->super.refCtr_ &= (uint8_t)(~TE_WAS_DISARMED & 0xFFU);
 
         QS_BEGIN_NOCRIT_PRE_(QS_QF_TIMEEVT_DISARM_ATTEMPT,
                          QS_priv_.locFilter[TE_OBJ], me)
